a2bdf8ba5166a6b07a78d3798defff966725b930
AntoninJuquel <antonin.juquel@outlook.fr>
2023-05-24 18:01:33 +0200

    package: added babel env transform and babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
+const moduleResolverConfig = [
+  'module-resolver',
+  {
+    alias: {
+      '@': './src',
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+];
+
+const transformInlineEnviromentVariables = 'transform-inline-environment-variables';
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [moduleResolverConfig, transformInlineEnviromentVariables],
   };
 };
